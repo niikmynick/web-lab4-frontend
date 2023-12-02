dcd7c82ee3f4a4e791e7d0cf4a52f581981432ed
niik <fk724s@mail.ru>
2023-12-02 14:07:05 +0300

    add App file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './templates/Header';
+import ResultsTable from "./templates/ResultsTable";
+import TopDiv from "./templates/TopDiv";
+import ClearButton from './templates/ClearButton';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Header />
+        <TopDiv />
+        <div id="results-div">
+          <ResultsTable />
+          <ClearButton />
+        </div>
+      </div>
   );
 }
 
